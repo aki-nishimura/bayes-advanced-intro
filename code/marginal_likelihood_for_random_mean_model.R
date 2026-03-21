cd29af23d91b8123182db76cf203ad6404809861
Aki Nishimura <aki.nishimura@jhu.edu>
2026-03-21 12:52:45 -0400

    Add code to compare gauss and cauchy dist
    to illustrate how difficult to visual diagnose potential heavy tails in posteriors

--- code/marginal_likelihood_for_random_mean_model.R
+++ code/marginal_likelihood_for_random_mean_model.R
@@ -61,3 +61,72 @@ plot(
 
 if (save_to_pdf) { dev.off() }
 
+
+
+## Plot Gaussian and Cauchy density 
+source("colors.R")
+
+kl_divergence <- function(cauchy_scale) {
+  integrand <- function(x) {
+    p <- dnorm(x)
+    q <- dcauchy(x, scale = cauchy_scale)
+    ifelse(p > 0, p * log(p / q), 0)
+  }
+  integrate(integrand, -Inf, Inf)$value
+}
+
+# Total variation distance, which technically is infinite if integrated over the entire real.
+tv_distance <- function(gamma, integ_width=10) {
+  integrand <- function(x) {
+    abs(dnorm(x) - dcauchy(x, scale = gamma))
+  }
+  0.5 * integrate(integrand, -integ_width, integ_width)$value
+}
+
+# Minimize KL divergence (or total variation)
+distance <- kl_divergence
+result <- optimize(distance, interval = c(0.1, 10))
+argmin_scale <- result$minimum
+
+# Plot
+save_to_pdf <- TRUE
+
+if (save_to_pdf) { 
+  filename <- sprintf(
+    "gaussian_cauchy_comparison_plot.pdf", nrow(sat_data)
+  )
+  filepath <- file.path("..", "lecture", "Figure", filename)
+  pdf(filepath, width = 6.5, height = 4.5) 
+  par(mar = c(4.1, 5.1, 2.1, 2.1)) # bottom, left, top, right
+}
+
+x <- seq(-10, 10, length.out = 1001)
+y_gaussian <- dnorm(x)
+y_cauchy   <- dcauchy(x, scale = argmin_scale)
+
+gaussian_color <- jhu_color$heritageBlue
+cauchy_color <- jhu_color$redOrange
+
+density_lwd <- 1.5
+plot(
+  x, y_cauchy, 
+  type = "l", 
+  lwd = density_lwd,
+  col = cauchy_color, 
+  xlab = expression(x), ylab = "Density",
+  frame = FALSE,
+  cex.main = 1.4,
+  cex.lab = 1.5,  
+  cex.axis = 1.3
+)
+lines(x, y_gaussian, col = gaussian_color, lwd = density_lwd)
+legend(
+  "topright", 
+  legend = c("Gaussian", paste0("Cauchy\n(scale = ", round(argmin_scale, 2), ")")),
+  col = c(gaussian_color, cauchy_color), 
+  lwd = 2,
+  bty = "n",
+  cex = 1.3
+)
+
+if (save_to_pdf) { dev.off() }
